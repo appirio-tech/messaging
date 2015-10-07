1aacac513f9d6cde14a4c2351e28e5d7c9dc783b
Alex Tran <vietnogi@gmail.com>
2015-10-07 15:04:59 -0700

    threads new design build

--- dist/lib.js
+++ dist/lib.js
@@ -38336,9 +38336,9 @@ $templateCache.put("views/selected-button.directive.html","<button ng-class=\"{\
 
   srv = function($resource, API_URL) {
     var methods, params, url;
-    url = API_URL + '/v3/threads/:threadId/messages';
+    url = API_URL + '/v3/messages/:id';
     params = {
-      threadId: '@threadId'
+      id: '@id'
     };
     methods = {
       put: {
